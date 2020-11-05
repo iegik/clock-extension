7a4cf598f46109e6ccb11a2abce77dc4b5020631
Arturs Jansons <a.jansons@gmail.com>
2020-11-05 03:17:43 +0300

    feat: Optimize for better performance

--- src/background.js
+++ src/background.js
@@ -1,28 +1,4 @@
 (() => {
-    const applyStyle = (el, style) => {
-        const arr = Object.keys(style);
-        arr.forEach(prop => {
-            el.style[prop] = style[prop];
-        });
-    }
-
-    const createArrow = ({ element, offset, length, width, color, baseSize }) => {
-        applyStyle(element, {
-            backgroundImage: `linear-gradient(90deg,
-                rgba(0,0,0,0) 0%,
-                rgba(0,0,0,0) ${offset * 100}%,
-                ${color} ${offset * 100}%,
-                ${color} ${(offset + length) * 100}%,
-                rgba(0,0,0,0) ${(offset + length) * 100}%
-            )`,
-            width: `${Math.round(baseSize)}px`,
-            height: `${Math.round(baseSize * width)}px`,
-            position: 'absolute',
-            top: `${Math.round((baseSize - baseSize * width)/2)}px`,
-            transformOrigin: `center center`,
-        });
-    }
-    
     const isNightMode = () => {
         const now = (new Date).getHours();
 
@@ -39,22 +15,32 @@
         'DS-Digital, digital, sans-serif',
         'Elektra, digital, sans-serif',
     ];
-    const fontFamily = fonts[1];
+    const colors = [
+        'rgb(200, 127, 127, 0.5)',
+        'rgba(127,127,127,0.5)',
+        'rgba(70,180,70,0.5)',
+        'rgba(127,127,127,0.2)',
+    ];
     const options = {
         isDigital: false,
+        fontFamily: fonts[0],
+        nightModeColor: colors[0],
+        dayModeColor: colors[1],
     }
 
+    const { isDigital, nightModeColor, dayModeColor, fontFamily } = options;
+
     let drawer;
-    if (options.isDigital) {
+    if (isDigital) {
         const color = isNightMode()
-            ? 'rgba(70,180,70,0.5)'
-            : 'rgba(127,127,127,0.2)';
-        const dgitalTime = document.createElement('div');
-        applyStyle(dgitalTime, {
-            position: 'absolute',
-            top: `calc(60vh - ${size/2}px)`,
-            left: `-50%`,
-            marginLeft: `calc(100% - 2.5ex)`,
+            ? nightModeColor
+            : dayModeColor;
+        const digitalTime = document.createElement('div');
+        Object.assign(digitalTime.style, {
+            // position: 'absolute',
+            // top: `calc(60vh - ${size/2}px)`,
+            // left: `-50%`,
+            // marginLeft: `calc(100% - 2.5ex)`,
             color,
             fontFamily,
             fontDisplay: 'swap',
@@ -65,21 +51,21 @@
             //     rgba(70,180,70,0.5) 0px 0.02em 0.05em,
             //     rgba(70,180,70,0.5) 0px 0.04em 0.15em`,
         });
-    
+
         const changeDigitalTime = time => {
-            dgitalTime.innerText = time;
+            digitalTime.innerText = time;
         }
-    
+
         const drawDigitalTime = () => {
             const now = new Date();
-            // const miliseconds = now.getMilliseconds();
-            // const time = `${now.toLocaleTimeString()}:${miliseconds}`;
+            // const milliseconds = now.getMilliseconds();
+            // const time = `${now.toLocaleTimeString()}:${milliseconds}`;
             const time = now.toLocaleTimeString();
             changeDigitalTime(time);
         }
 
         drawer = drawDigitalTime;
-        document.body.appendChild(dgitalTime);
+        document.body.appendChild(digitalTime);
     } else {
         const color = isNightMode()
             ? 'rgb(200, 127, 127, 0.5)'
@@ -88,93 +74,119 @@
         const hoursArrow = document.createElement('div');
         const minutesArrow = document.createElement('div');
         const secondsArrow = document.createElement('div');
-    
-        applyStyle(clock, {
+
+        const memoize = (fn) => {
+            const cache = new Map();
+
+            return (arg) => {
+                if(cache.get(arg)) return cache.get(arg);
+                return cache.set(arg, fn(arg)).get(arg);
+            }
+        }
+
+        const createArrow = ({ element, offset, length, width, color, baseSize }) => {
+            Object.assign(element.style, {
+                backgroundImage: `linear-gradient(90deg,
+                    rgba(0,0,0,0) 0%,
+                    rgba(0,0,0,0) ${offset * 100}%,
+                    ${color} ${offset * 100}%,
+                    ${color} ${(offset + length) * 100}%,
+                    rgba(0,0,0,0) ${(offset + length) * 100}%
+                )`,
+                width: `${Math.round(baseSize)}px`,
+                height: `${Math.round(baseSize * width)}px`,
+                position: 'absolute',
+                top: `${Math.round((baseSize - baseSize * width)/2)}px`,
+                transformOrigin: `center center`,
+            });
+        }
+
+        const changeArrow = (arrow, value) => {
+            if (arrow.style.transform === value) return;
+            arrow.style.transform = value;
+        }
+
+        Object.assign(clock.style, {
             position: 'relative',
             //background: `${color}`,
             borderRadius: `50%`,
             //border: `${color} solid ${Math.round(size * 0.02)}px`,
             width: `${size}px`,
             height: `${size}px`,
-            top: `calc(50vh - ${size/2}px)`,
-            left: `calc(50vw - ${size/2}px)`,
+            // top: `calc(50vh - ${size/2}px)`,
+            // left: `calc(50vw - ${size/2}px)`,
         });
-    
+
         createArrow({
             element: hoursArrow,
             offset: 0.45,
-            length: 0.25, 
-            width: 0.015, 
-            color, 
-            baseSize: size 
+            length: 0.25,
+            width: 0.015,
+            color,
+            baseSize: size
         });
-    
+
         createArrow({
-            element: minutesArrow, 
+            element: minutesArrow,
             offset: 0.45,
-            length: 0.40, 
-            width: 0.01, 
-            color, 
-            baseSize: size 
+            length: 0.40,
+            width: 0.01,
+            color,
+            baseSize: size
         });
-    
+
         createArrow({
-            element: secondsArrow, 
+            element: secondsArrow,
             offset: 0.45,
-            length: 0.50, 
-            width: 0.005, 
-            color, 
-            baseSize: size 
+            length: 0.50,
+            width: 0.005,
+            color,
+            baseSize: size
         });
-        
+
         for (let deg = 0; deg < 360; deg += 30) {
             const hourDot = document.createElement('div');
             createArrow({
-                element: hourDot, 
+                element: hourDot,
                 offset: 0,
-                length: 0.01, 
-                width: 0.01, 
-                color, 
-                baseSize: size 
+                length: 0.01,
+                width: 0.01,
+                color,
+                baseSize: size
             });
             hourDot.style.transform = `rotate(${deg - 90}deg)`;
             clock.appendChild(hourDot);
         }
-    
+
         for (let deg = 0; deg < 360; deg += 6) {
             if (deg % 30 === 0) continue;
             const hourDot = document.createElement('div');
             createArrow({
-                element: hourDot, 
+                element: hourDot,
                 offset: 0,
-                length: 0.005, 
-                width: 0.005, 
-                color, 
-                baseSize: size 
+                length: 0.005,
+                width: 0.005,
+                color,
+                baseSize: size
             });
             hourDot.style.transform = `rotate(${deg - 90}deg)`;
             clock.appendChild(hourDot);
         }
-    
-        const changeHours = hours => {
-            hoursArrow.style.transform = `rotate(${360 / 12 * hours - 90}deg)`;
-        }
-        const changeMinutes = minutes => {
-            minutesArrow.style.transform = `rotate(${360 / 60 * minutes - 90}deg)`;
-        }
-        const changeSeconds = seconds => {
-            secondsArrow.style.transform = `rotate(${360 / 60 * seconds - 90}deg)`;
-        }
-    
+
+        const calculateHours = memoize(hours => `rotate(${360 / 12 * hours - 90}deg)`);
+        const calculateMinutes = memoize(minutes => `rotate(${360 / 60 * minutes - 90}deg)`);
+        const calculateSeconds = memoize(seconds => `rotate(${360 / 60 * seconds - 90}deg)`);
+
         const drawAnalogueTime = () => {
             const now = new Date();
-            const miliseconds = now.getMilliseconds();
-            const seconds = now.getSeconds() + miliseconds * 0.001;
+            const milliseconds = now.getMilliseconds();
+            const seconds = now.getSeconds() + milliseconds * 0.001;
             const minutes = now.getMinutes() + 0.017 *  seconds;
             const hours = now.getHours() + 0.017 * minutes;
-            changeHours(hours);
-            changeMinutes(minutes);
-            changeSeconds(seconds);
+
+            changeArrow(hoursArrow, calculateHours(hours))
+            changeArrow(minutesArrow, calculateMinutes(minutes))
+            changeArrow(secondsArrow, calculateSeconds(seconds))
         }
 
         drawer = drawAnalogueTime;
